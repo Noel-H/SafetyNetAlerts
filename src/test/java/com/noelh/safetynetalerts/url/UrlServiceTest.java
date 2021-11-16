cbac863a76e26e81196d7d275d912c05c2b45c23
Noel <noel.huberto@gmail.com>
2021-11-16 05:08:34 +0100

    UrlServiceTest Class creation

---
+++ src/test/java/com/noelh/safetynetalerts/url/UrlServiceTest.java
@@ -0,0 +1,36 @@
+package com.noelh.safetynetalerts.url;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UrlServiceTest {
+
+    @Test
+    public void getPersonsListByStationIdTest_Sould_Return_FireStationUrlResponse() {
+    }
+
+    @Test
+    public void getChildListByAddressTest_Should_Return_ChildAlertUrlResponseList() {
+    }
+
+    @Test
+    public void getPhonesByStationIdTest_Should_Return_PhoneUrlResponseList() {
+    }
+
+    @Test
+    public void getPersonAndStationNumberByAddressTest_Should_Return_FireUrlWithStationNumberResponse() {
+    }
+
+    @Test
+    public void getPersonsGroupedByAddressTest_Should_Return_MapList() {
+    }
+
+    @Test
+    public void getPersonInfoByFirstNameAndLastNameTest_Should_Return_PersonInfoUrlResponseList() {
+    }
+
+    @Test
+    public void getMailsByCityTest_Should_Return_CommunityEmailUrlResponseList() {
+    }
+}
